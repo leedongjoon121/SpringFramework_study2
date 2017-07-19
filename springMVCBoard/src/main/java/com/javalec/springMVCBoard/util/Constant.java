23017992f6ffd8cd43c6b3f5867581da5d89b9b6
leedongjoon121 <ehdwns46@naver.com>
2017-07-19 18:15:23 +0900

    upload spring framework Study,

---
+++ springMVCBoard/src/main/java/com/javalec/springMVCBoard/util/Constant.java
@@ -0,0 +1,9 @@
+package com.javalec.springMVCBoard.util;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Constant {
+
+	public static JdbcTemplate template;  // 어디서든 쓸 수 있게 static으로 할당 해놓음
+	
+}
